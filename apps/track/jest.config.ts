a20fcc463ac7594b6d152eda5cf53b769e821be7
mengmeng123 <wm11282414@gmail.com>
2025-09-23 16:56:42 +0800

    init

---
+++ apps/track/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@idg/track',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/track',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
