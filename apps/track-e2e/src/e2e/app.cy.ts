970ba7676590165ab3eb4d2bc17c13108213f49e
mengmeng123 <wm11282414@gmail.com>
2025-09-25 10:35:37 +0800

    fix:ts

--- apps/track-e2e/src/e2e/app.cy.ts
+++ apps/track-e2e/src/e2e/app.cy.ts
@@ -5,7 +5,7 @@ describe('@idg/track-e2e', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    //@ts-ignore
+    //@ts-expect-error
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
